e0d473a7e967efccce5970c74391e790f02a7e97
nick defoe <jafoca@resurgam.local>
2015-09-29 15:41:52 +1000

    update gems, app to be able to re-build

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'sinatra-flash', :require => 'sinatra/flash'
 
 # Component requirements
 gem 'bcrypt-ruby', :require => "bcrypt"
-gem 'rack-less'
+gem 'rack-less', "~>2.0.2"
 gem 'erubis', "~> 2.7.0"
 gem 'mongo_mapper'
 gem 'bson_ext', :require => "mongo"
@@ -22,7 +22,7 @@ gem 'tzinfo'
 gem "activesupport"
 
 # Padrino Stable Gem
-gem 'padrino', '0.10.5'
+gem 'padrino', '0.11.4'
 
 # Or Padrino Edge
 # gem 'padrino', :git => 'git://github.com/padrino/padrino-framework.git'
--- config/database.rb
+++ config/database.rb
@@ -3,11 +3,11 @@ MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10018, :logg
 case Padrino.env
   when :development then
     MongoMapper.database = 'app3138678'
-    MongoMapper.database.authenticate('heroku','bluesky120')
-  
-  when :production  then 
+    MongoMapper.database.authenticate('dev','gillespiegroup')
+
+  when :production  then
     MongoMapper.database = 'app3138678'
     MongoMapper.database.authenticate('heroku','bluesky120')
-  
+
   when :test        then MongoMapper.database = 'signcontrol_test'
 end
